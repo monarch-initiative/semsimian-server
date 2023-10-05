b533a65c937c5134b1dbca46b39f63189e9db3e1
Kevin Schaper <kevinschaper@gmail.com>
2023-10-05 12:58:25 -0700

    Uses lazy-static to share the RustSemsimian instance, runs a small compare query to initialize the RSS instance. Changes to 127.0.0.1 from 0.0.0.0.

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ use serde::{
 };
 use semsimian::termset_pairwise_similarity::TermsetPairwiseSimilarity;
 use semsimian::{Predicate, RustSemsimian, TermID};
+use lazy_static::lazy_static;
 
 //--- STRUCTS ---//
 struct Tsps(TermsetPairwiseSimilarity);
@@ -48,13 +49,31 @@ impl Serialize for Tsps {
     }
 }
 
+lazy_static! {
+    static ref RSS: RustSemsimian = get_rss_instance();
+}
 
 #[get("/compare/<termset1>/<termset2>")]
 fn compare_termsets(termset1: String, termset2: String) -> Json<Tsps> {
     // Compare two termsets, each represented as a comma-separated set of term IDs
     // Return a TermsetPairwiseSimilarity object
 
-    // get path to phenio.db
+    // split termset1 and termset2 into vectors of TermIDs
+    let mut terms1: HashSet<TermID> = HashSet::new();
+    for term in termset1.split(",") {
+        terms1.insert(term.to_string());
+    }; 
+    let mut terms2: HashSet<TermID> = HashSet::new();
+    for term in termset2.split(",") {
+        terms2.insert(term.to_string());
+    };
+    println!("Termset 1: {:?}", terms1);
+    println!("Termset 2: {:?}", terms2);
+    let result = RSS.termset_pairwise_similarity(&terms1, &terms2, &None);
+    Json(Tsps(result))
+}
+
+fn get_rss_instance() -> RustSemsimian {
     let mut db_path = PathBuf::new();
     if let Some(home) = std::env::var_os("HOME") {
         db_path.push(home);
@@ -72,20 +91,7 @@ fn compare_termsets(termset1: String, termset2: String) -> Json<Tsps> {
 
     let mut rss = RustSemsimian::new(None, predicates, None, db);
     rss.update_closure_and_ic_map();
-
-    // split termset1 and termset2 into vectors of TermIDs
-    let mut terms1: HashSet<TermID> = HashSet::new();
-    for term in termset1.split(",") {
-        terms1.insert(term.to_string());
-    }; 
-    let mut terms2: HashSet<TermID> = HashSet::new();
-    for term in termset2.split(",") {
-        terms2.insert(term.to_string());
-    };
-    println!("Termset 1: {:?}", terms1);
-    println!("Termset 2: {:?}", terms2);
-    let result = rss.termset_pairwise_similarity(&terms1, &terms2, &None);
-    Json(Tsps(result))
+    rss
 }
 
 // this is our get route which will be requested at the "/" location wherever it is mounted
@@ -98,5 +104,10 @@ fn say_hello() -> &'static str {
 // or just leave it as "/" as the default location
 #[launch]
 fn rocket() -> _ {
+    // run a first compare to warm up the RustSemsimian instance
+    RSS.termset_pairwise_similarity(
+        &HashSet::from(["MP:0010771".to_string()]),
+        &HashSet::from(["HP:0004325".to_string()]),
+        &None,);
     rocket::build().mount("/", routes![say_hello, compare_termsets])
 }
