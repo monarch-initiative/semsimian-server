262afcb3cef9c03f37919d8acd9e211c5988278d
Glass <glass.ships@outlook.com>
2023-12-11 15:46:25 -0700

    Update bin.rs

--- src/bin.rs
+++ src/bin.rs
@@ -7,8 +7,8 @@ use semsimian_server::{compare_termsets, search, say_hello};
 
 #[launch]
 pub fn rocket() -> _ {
-    // compare_termsets("HP:0000001,HP:0000002", "HP:0000003,HP:0000004");
-    // search("HP:0000001,HP:0000002", "ZFIN", Some(10));
+    compare_termsets("HP:0000001,HP:0000002", "HP:0000003,HP:0000004");
+    search("HP:0000001,HP:0000002", "ZFIN", Some(10));
     search("HP:0000001,HP:0000002", "ZFIN", 9);
     rocket::build().mount("/", routes![
         say_hello,
